6e910492b34f6e77f50bd217f4905ae797aeab45
sucixyz <sucirahmasafitri10@gmail.com>
2025-04-24 19:56:31 +0700

    stream-protein.py

--- stream-protein.py
+++ stream-protein.py
@@ -38,5 +38,5 @@ elif 14 <= usia <= 18:
 elif usia >= 19:
     if jenis_kelamin.lower() == 'perempuan':
         return 56
-    else:
-        print "Data tidak valid."
+else:
+    return"Usia tidak valid."
